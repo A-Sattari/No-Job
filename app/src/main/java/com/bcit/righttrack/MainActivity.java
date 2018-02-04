9e08d46068734290813a355b4fe4526484e4d70d
Justin Heidema <jrheidema@gmail.com>
2018-02-04 15:42:52 -0800

    Configured bottom nav to visit appropriate fragments

--- app/src/main/java/com/bcit/righttrack/MainActivity.java
+++ app/src/main/java/com/bcit/righttrack/MainActivity.java
@@ -1,8 +1,10 @@
 package com.bcit.righttrack;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
@@ -14,15 +16,32 @@ import android.widget.TextView;
  * @version pre-alpha
  * @author Group JAD
  */
-public class MainActivity extends AppCompatActivity {
+public class MainActivity
+        extends AppCompatActivity
+        implements FoodBank.OnFragmentInteractionListener,
+                   HomePage.OnFragmentInteractionListener,
+                   HousingPage.OnFragmentInteractionListener,
+                   TrainingPage.OnFragmentInteractionListener{
 
     private TextView mTextMessage;
+
+    // Declaring Fragment objects
+    private FragmentTransaction ft;
+    private Fragment currentFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         /**************************************/
+
+        // Initializng fragment manager
+        ft = getSupportFragmentManager().beginTransaction();
+
+        // Setting the current fragment to the Home Page fragment upon startup
+        currentFragment = new HomePage();
+        ft.replace(R.id.container, currentFragment);
+        ft.commit();
 
         mTextMessage = findViewById(R.id.message);
         BottomNavigationView bottomNavBar = findViewById(R.id.navigation);
@@ -36,37 +55,27 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-
-            /*
-             These two lines below can be combined and written like this
-                  FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-             but to understand what is going on and how they work I leave them like that.
-             */
-            // Interface for interacting with Fragment objects inside of an Activity
-            FragmentManager fragManager = getSupportFragmentManager();
-            // Allows you to perform transactions, such as add and remove fragments
-            FragmentTransaction transaction = fragManager.beginTransaction();
-
             switch (item.getItemId()) {
 
+                // Switch to Home Fragment when home tab is pressed
                 case R.id.homeTab:
-                    // Replace an existing fragment that was added to a container
-                    transaction.replace(R.id.container, new HomePage());
-                    transaction.commit();
+                    switchToHomeFragment();
                     return true;
 
+                // Switch to Training Fragment when training tab is pressed
                 case R.id.tab2:
-                    mTextMessage.setText(R.string.title_dashboard);
+                    switchToTrainingFragment();
                     return true;
 
+                // Switch to Food Bank fragment when food tab is pressed
                 case R.id.tab3:
-                    mTextMessage.setText(R.string.title_notifications);
+                    switchToFoodFragment();
                     return true;
 
+                // Switch to Housing Fragment when housing tab is pressed
                 case R.id.foodBankTab:
-                    // Replace an existing fragment that was added to a container
-                    transaction.replace(R.id.container, new FoodBank());
-                    transaction.commit();
+                    switchToHousingFragment();
+
                     return true;
             }
             return false;
@@ -74,4 +83,32 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+
+    // Switches view to home page
+    public void switchToHomeFragment() {
+        ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.container, new HomePage()).commit();
+    }
+
+    // Switches view to food bank page
+    public void switchToFoodFragment() {
+        ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.container, new FoodBank()).commit();
+    }
+
+    // Switches view to housing page
+    public void switchToHousingFragment() {
+        ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.container, new HousingPage()).commit();
+    }
+
+    // Switches view to training page
+    public void switchToTrainingFragment() {
+        ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.container, new TrainingPage()).commit();
+    }
 }
