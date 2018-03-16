8cbda2db661403ae78f31e401c50e04dc11bbfc5
Alireza <A.Sattari@outlook.com>
2018-03-15 19:28:19 -0700

    Cleaned the code

--- app/src/main/java/com/bcit/righttrack/FoodBank.java
+++ app/src/main/java/com/bcit/righttrack/FoodBank.java
@@ -1,6 +1,5 @@
 package com.bcit.righttrack;
 
-import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
@@ -8,41 +7,28 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomSheetBehavior;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.CardView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.Button;
-import android.widget.LinearLayout;
-import android.widget.ListView;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.model.BitmapDescriptor;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-
+/**
+ *
+ * @author Alireza
+ */
 public class FoodBank extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-    private View foodBankView;
     private MapView mapView;
     private GoogleMap gMap;
     private Marker previousMarker = null;
@@ -50,132 +36,31 @@ public class FoodBank extends Fragment {
     private TextView fbName;
     private Button phoneButton;
     private Button directionButton;
-
     private OnFragmentInteractionListener mListener;
-
-    public FoodBank() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FoodBank.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static FoodBank newInstance(String param1, String param2) {
-        FoodBank fragment = new FoodBank();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         // Inflate the layout for this fragment
-        foodBankView = inflater.inflate(R.layout.food_bank, container, false);
+        final View foodBankView = inflater.inflate(R.layout.food_bank, container, false);
+        final View bottomSheet = foodBankView.findViewById(R.id.fb_bottom_sheet);
         mapView = foodBankView.findViewById(R.id.fbMapView);
-        mapView.onCreate(savedInstanceState);
-        mapView.onResume();
-
-        // Buttons
+        fbName = foodBankView.findViewById(R.id.fbName);
         directionButton = foodBankView.findViewById(R.id.fbDirection);
         phoneButton = foodBankView.findViewById(R.id.fbPhone);
 
-        // Bottom Sheet configuration
-        final View bottomSheet = foodBankView.findViewById(R.id.fb_bottom_sheet);
-        bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet);
-        bottomSheetBehavior.setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
-            @Override
-            public void onStateChanged(@NonNull View bottomSheet, int newState) {
-                if (newState == BottomSheetBehavior.STATE_HIDDEN) {
-                    previousMarker.setIcon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED));
-                }
-            }
-            @Override
-            public void onSlide(@NonNull View bottomSheet, float slideOffset) {}
-        });
-        bottomSheetBehavior.setHideable(true);
-        bottomSheetBehavior.setState(BottomSheetBehavior.STATE_HIDDEN);
-
-        fbName = foodBankView.findViewById(R.id.fbName);
+        mapView.onCreate(savedInstanceState);
+        mapView.onResume();
 
         // Google Map View Configuration
-        mapView.getMapAsync(new OnMapReadyCallback() {
-
-            @Override
-            public void onMapReady(GoogleMap mMap) {
-                gMap = mMap;
-
-                // For dropping a marker at a point on the Map
-                LatLng location = new LatLng(49.203496743701606, -122.90963686974206);
-                LatLng location2 = new LatLng(49.22491835146481, -122.91048369358666);
-                LatLng location3 = new LatLng(49.21491835146481, -122.91048369358666);
-                gMap.addMarker(new MarkerOptions().position(location).title("Holy Trinity Anglican Cathedral - Breakfast Program").snippet("(604)521-2511"));
-                gMap.addMarker(new MarkerOptions().position(location2).title("Marker2 Title").snippet("(604)521-2511"));
-                gMap.addMarker(new MarkerOptions().position(location3).title("Marker3 Title").snippet("(604)517-6168"));
-
-                // For zooming automatically to the location of the marker
-                CameraPosition cameraPosition = new CameraPosition.Builder().target(location).zoom(12).build();
-                gMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
-
-                // Marker click listener
-                gMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
-                    @Override
-                    public boolean onMarkerClick(Marker marker) {
-                        marker.hideInfoWindow();
-
-                        // If the marker wasn't click, change its color
-                        if (previousMarker == null) {
-                            marker.setIcon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE));
-
-                        } else { // If the marker was clicked before, then resets it color to red
-                            previousMarker.setIcon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED));
-                            marker.setIcon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE));
-                        }
-                        previousMarker = marker;
-
-                        bottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
-                        fbName.setText(marker.getTitle());
-
-                        // Buttons
-                        directionButtonListener(directionButton, 49.203496743701606, -122.90963686974206);
-                        phoneButtonListener(phoneButton, marker.getSnippet());
-
-                        return true;
-                    }
-                });
-
-                // Map click listener
-                gMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
-                    @Override
-                    public void onMapClick(LatLng latLng) {
-
-                        bottomSheetBehavior.setHideable(true);
-                        bottomSheetBehavior.setState(BottomSheetBehavior.STATE_HIDDEN);
-                        // Resets the marker color when user clicks on the map
-                        if (previousMarker != null)
-                            previousMarker.setIcon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED));
-                    }
-                });
-            }
-
-        });
+        setGoogleMapConfiguration();
+        // Bottom Sheet configuration
+        setBottomSheetBehavior(bottomSheet);
 
         return foodBankView;
     }
@@ -244,7 +129,110 @@ public class FoodBank extends Fragment {
     }
 
     /**
-     * Direction button listener.
+     * Defines the Google Map view configuration.
+     * Calls the marker and map on click listener method.
+     */
+    private void setGoogleMapConfiguration() {
+
+        mapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap mMap) {
+                gMap = mMap;
+
+                // For dropping a marker at a point on the Map
+                LatLng location = new LatLng(49.203496743701606, -122.90963686974206);
+                LatLng location2 = new LatLng(49.22491835146481, -122.91048369358666);
+                LatLng location3 = new LatLng(49.21491835146481, -122.91048369358666);
+                gMap.addMarker(new MarkerOptions().position(location).title("Holy Trinity Anglican Cathedral - Breakfast Program").snippet("(604)521-2511"));
+                gMap.addMarker(new MarkerOptions().position(location2).title("Marker2 Title").snippet("(604)521-2511"));
+                gMap.addMarker(new MarkerOptions().position(location3).title("Marker3 Title").snippet("(604)517-6168"));
+
+                // For zooming automatically to the location of the marker
+                CameraPosition cameraPosition = new CameraPosition.Builder().target(location).zoom(12).build();
+                gMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+
+                // Marker click listener
+                markerOnClickListener();
+                // Map click listener
+                mapOnClickListener();
+            }
+
+        });
+    }
+
+    /**
+     * Changes the color of the markers and calls the bottom sheet buttons.
+     */
+    private void markerOnClickListener() {
+
+        gMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+            @Override
+            public boolean onMarkerClick(Marker marker) {
+                marker.hideInfoWindow();
+
+                // If the marker wasn't click, change its color
+                if (previousMarker == null) {
+                    marker.setIcon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE));
+
+                } else { // If the marker was clicked before, then resets it color to red
+                    previousMarker.setIcon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED));
+                    marker.setIcon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE));
+                }
+                previousMarker = marker;
+
+                bottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+                fbName.setText(marker.getTitle());
+
+                // Buttons
+                directionButtonListener(directionButton, 49.203496743701606, -122.90963686974206);
+                phoneButtonListener(phoneButton, marker.getSnippet());
+
+                return true;
+            }
+        });
+    }
+
+    /**
+     * Resets the marker color and hides the bottom sheet.
+     */
+    private void mapOnClickListener() {
+
+        gMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+
+                bottomSheetBehavior.setHideable(true);
+                bottomSheetBehavior.setState(BottomSheetBehavior.STATE_HIDDEN);
+                // Resets the marker color when user clicks on the map
+                if (previousMarker != null)
+                    previousMarker.setIcon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED));
+            }
+        });
+    }
+
+    /**
+     * Configures the bottom sheet behavior
+     * @param btSheetView bottom sheet view
+     */
+    private void setBottomSheetBehavior(View btSheetView) {
+
+        bottomSheetBehavior = BottomSheetBehavior.from(btSheetView);
+        bottomSheetBehavior.setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
+            @Override
+            public void onStateChanged(@NonNull View bottomSheet, int newState) {
+                if (newState == BottomSheetBehavior.STATE_HIDDEN) {
+                    previousMarker.setIcon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED));
+                }
+            }
+            @Override
+            public void onSlide(@NonNull View bottomSheet, float slideOffset) {}
+        });
+        bottomSheetBehavior.setHideable(true);
+        bottomSheetBehavior.setState(BottomSheetBehavior.STATE_HIDDEN);
+    }
+
+    /**
+     * Opens Google Map in navigation mode.
      * @param direction button
      */
     private void directionButtonListener(final Button direction, final double latY, final double longX) {
@@ -253,7 +241,6 @@ public class FoodBank extends Fragment {
         direction.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // Opens Google Map in navigation mode
                 Intent gMapDir = new Intent(Intent.ACTION_VIEW, geoUri);
                 startActivity(gMapDir);
 
@@ -269,7 +256,7 @@ public class FoodBank extends Fragment {
     }
 
     /**
-     * Phone button listener.
+     * Opens the phone's default dialer.
      * @param phone button
      */
     private void phoneButtonListener(final Button phone, final String phoneNumber) {
